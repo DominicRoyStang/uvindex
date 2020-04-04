30290940e23309f5de15bb553e3d15fab019401e
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-04-04 15:16:29 -0400

    App engine terraform deployment

---
+++ terraform/outputs.tf
@@ -0,0 +1,3 @@
+output "app_id" {
+    value = google_app_engine_application.app.app_id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "gcp_project_id" {
+    default = "uvindex-dominicroystang"
+}
+
+variable "project_name" {
+    default = "uvindex"
+}
+
+variable "gcp_region" {
+    default = "us-central1"
+}
+
+variable "gcp_zone" {
+    default = "us-central-1-c"
+}
+
+variable "gcp_location" {
+    default = "us-central"
+}
+
+variable "domain_name" {
+    default = "uvindex.xyz"
+}
