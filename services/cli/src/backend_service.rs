d272053fb102983bdefd0a15ae1c10d039d9face
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-04-12 04:10:15 -0400

    Use http by default in the cli.

--- services/cli/src/backend_service.rs
+++ services/cli/src/backend_service.rs
@@ -8,7 +8,7 @@ struct BackendResponse {
 }
 
 pub fn current_uv_index(latitude: f32, longitude: f32) -> Result<f32, Box<dyn Error>> {
-    let backend_protocol = env::var("UVINDEX_BACKEND_PROTOCOL").unwrap_or(String::from("https"));
+    let backend_protocol = env::var("UVINDEX_BACKEND_PROTOCOL").unwrap_or(String::from("http"));
     let backend_host = env::var("UVINDEX_BACKEND_HOST").unwrap_or(String::from("uvindex.xyz"));
     let backend_port = env::var("UVINDEX_BACKEND_PORT").unwrap_or(String::from("80"));
     let backend_url = format!("{}://{}:{}", backend_protocol, backend_host, backend_port);
